310e25bb8aab58c298f876a336a9890f8ae24453
kvnstv1 <kvnstv1@gmail.com>
2025-03-10 17:10:52 +1300

    Steve's help

--- Align.py
+++ Align.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.decomposition import PCA
 from scipy.spatial.transform import Rotation as R
 import open3d as o3d
+from scipy.linalg import det
 
 file_path_source = None
 file_path_target = None
@@ -56,9 +57,34 @@ pca2.fit(translated_points2)
 
 axes1 = pca1.components_
 axes2 = pca2.components_
+if (det(axes1) < 0):
+    axes1[2,:] *= -1
+if (det(axes2) < 0):
+    axes2[2,:] *= -1
+
+print("Axes 1")
+print(str(axes1))
+
+print("axes 1 det")
+
+print(det(axes1))
+
+
+print("Axes 2")
+print(str(axes2))
+      
+print("axes 2 det")
+
+print(det(axes2))
+
 
 # Align principal axes
 rotation = R.align_vectors(axes1, axes2)[0]
+
+print("R")
+print((rotation.as_matrix()))
+print("R det")
+#print(det(rotation))
 
 # Apply rotation and translation
 aligned_points1 = np.dot(scaled_points1, rotation.as_matrix())
@@ -69,7 +95,7 @@ final_aligned_points1 = aligned_points1 + centroid2
 # Visualize
 aligned_pcd1 = o3d.geometry.PointCloud()
 aligned_pcd1.points = o3d.utility.Vector3dVector(final_aligned_points1)
-#aligned_pcd1.paint_uniform_color([1, 0, 0])  # Paint red
-#pcd2.paint_uniform_color([0, 1, 0])  # Paint green
+aligned_pcd1.paint_uniform_color([1, 0, 0])  # Paint red
+pcd2.paint_uniform_color([0, 1, 0])  # Paint green
 
 o3d.visualization.draw_geometries([pcd2, aligned_pcd1])
