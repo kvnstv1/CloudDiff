56216c11774c662af18ce7212e625606c9f92062
kvnstv1 <kvnstv1@gmail.com>
2025-04-07 20:23:59 +1200

    Unit Test 2 completed.

--- RegistrationNewAttempt/Main.py
+++ RegistrationNewAttempt/Main.py
@@ -28,15 +28,64 @@ def selectFiles():
 
     return source_file_path, target_file_path
 
+def readPlyFile(filename):
+    """
+    Reads a ply file.
+    Returns a ndarray that is ready to be operated on
+    """
+
+    pcd = o3d.io.read_point_cloud(filename)
+    pcd = pcd.farthest_point_down_sample(50000)
+    data = np.asarray(pcd.points)
+    data = np.transpose(data)
+
+    return data
+
+def writePlyFile(data):
+    """
+    Writes a ply file with a given filename and data.
+    """
+    filename = filedialog.asksaveasfilename(
+        title="Save sampled PLY file as",
+        defaultextension=".ply",
+        filetypes=[("PLY files", "*.ply"), ("All files", "*.*")]
+    )
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(data.T)
+    o3d.io.write_point_cloud(filename, pcd, write_ascii=True)
+    print(f"Wrote data to {filename} successfully.")
 
 
+def centreAndScale(source, target):
+    """
+    Centres and scales the source and target
+    Assumes source and target are arrays of shape(3,n)
+    """
+    sourceCentroid = np.mean(source, axis=1)[:, np.newaxis]
+    targetCentroid = np.mean(target, axis=1)[:, np.newaxis]
+
+    source = source - sourceCentroid
+    target = target - targetCentroid
+
+    #Note: Using Euclidean norms
+    sourceDist = np.mean(np.linalg.norm(source, axis=0))
+    targetDist = np.mean(np.linalg.norm(target, axis=0))
+
+    scale = targetDist/sourceDist
+    source = source * scale
+
+    return source, target
 
 
 def main():
 
     sourceFile, targetFile = selectFiles()
+    source = readPlyFile(sourceFile)
+    target = readPlyFile(targetFile)
+    source, target = centreAndScale(source,target)
+    writePlyFile(source)
+    writePlyFile(target)
 
-    
 
 
 if __name__ == "__main__":
